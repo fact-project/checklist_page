f802330f9a6a39e37c2d3e736ac8d10d20b51f24
Dominik Neise <neised@phys.ethz.ch>
2016-10-24 11:10:50 +0100

    make nicer and shorter

--- compose_email_message.php
+++ compose_email_message.php
@@ -1,21 +1,30 @@
 <?php
 
-// for details about $_POST["ticked_checks"], c.f. form.php
+function is_ticked($name){
+    // This function returns the strings "True" or "False" 
+    // depending on whether the user 'ticked' the checkboxes.
+    // for details about $_POST["ticked_checks"], c.f. form.php
+    if ( in_array($name, $_POST["ticked_checks"])){
+        return "True";
+    } else {
+        return "False";
+    }
+};
 
 function compose_email_message() {
-    $file1name = "http://fact-project.org/Checklist/images/".date("Y")."/".date("m")."/IRcam_".date("Y-m-d_H:i:s").".jpg";
-    $file2name = "http://fact-project.org/Checklist/images/".date("Y")."/".date("m")."/FocalPlane_".date("Y-m-d_H:i:s").".jpg";
-
     $message = $_POST['Obsname'] . "has completed a shutdown form:\n\n";
+    $message .= "Telescope Parked:\t\t" . is_ticked("parked") . "\n";
+    $message .= "Bias Disconnected:\t\t" . is_ticked("bias_off") . "\n";
+    $message .= "Lid Closed:\t\t\t" . is_ticked("lid_closed") . "\n";
+    $message .= "TPoint Camera Off:\t" . is_ticked("tpoint_off") . "\n";
+    $message .= "Drive Off:\t\t\t" . is_ticked("drive_off") . "\n";
 
-    $message = $message . "Telescope Parked:\t\t" . in_array("parked", $_POST["ticked_checks"]) . "\n";
-    $message = $message . "Bias Disconnected:\t\t" . in_array("bias_off", $_POST["ticked_checks"]) . "\n";
-    $message = $message . "Lid Closed:\t\t\t" . in_array("lid_closed", $_POST["ticked_checks"]) . "\n";
-    $message = $message . "TPoint Camera Off:\t" . in_array("tpoint_off", $_POST["ticked_checks"]) . "\n";
-    $message = $message . "Drive Off:\t\t\t" . in_array("drive_off", $_POST["ticked_checks"]) . "\n";
-    $message = $message . "The focal plane image available at:\n $file2name\n";
-    $message = $message . "The IR Cam image available at:\n $file1name\n\n";
-    $message = $message . "They affirm to the information.\n";
+    $image_base_path = "http://fact-project.org/Checklist/images/".date("Y")."/".date("m");
+    $file1name = $image_base_path . "/IRcam_".date("Y-m-d_H:i:s").".jpg";
+    $file2name = $image_base_path . "/FocalPlane_".date("Y-m-d_H:i:s").".jpg";
+    $message .= "The focal plane image available at:\n $file2name\n";
+    $message .= "The IR Cam image available at:\n $file1name\n\n";
+    $message .= "They affirm to the information.\n";
 
     return $message;
 };
