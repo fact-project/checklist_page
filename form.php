16405140427d3921f741b08384fa0d2e60e0a1c2
Dominik Neise <neised@phys.ethz.ch>
2017-04-04 15:51:15 +0200

    add col titles; add reaction; remove email

--- form.php
+++ form.php
@@ -15,24 +15,35 @@
 
       <table style="width:600">
         <tr>
+          <td>Check</td>
+          <td>Status</td>
+          <td> Checked? </td>
+          <td> Reaction </td>
+        </tr>
+        <tr>
           <td>Telescope Parked?</td>
           <td><img src="../cam/ircam_with_date.jpg" width="320" alt="cam picture" /></td>
           <td> <input type="checkbox" name="ticked_checks[]" value="parked"> </td>
+          <td> Call expert! --> Call MAGIC shift leader!</td>
         </tr>
         <tr>
           <td>Drive Locked?</td>
           <td><?php echo drive_status(); ?></td>
           <td> <input type="checkbox" name="ticked_checks[]" value="is_locked"> </td>
+          <td> Call expert </td>
         </tr>
         <tr>
           <td>Bias Disconnected</td>
           <td><?php echo bias_status(); ?></td>
           <td> <input type="checkbox" name="ticked_checks[]" value="bias_off"> </td>
+          <td> check <a href="http://www.fact-project.org/showlog/index.php?log=biasctrl#bottom">showlog biasctrl</a>
+          if bias was "VoltageOff", before "Disconnected"; if yes, all good just report via email; if not call expert. </td>
         </tr>
         <tr>
           <td>Trigger Valid</td>
           <td><?php echo trigger_status(); ?></td>
           <td> <input type="checkbox" name="ticked_checks[]" value="trigger_off"> </td>
+          <td> report via email to fact-online </td>
         </tr>
         <tr>
           <td>Camera Lid Closed</td>
@@ -42,20 +53,23 @@
             <?php echo lid_status(); ?>
           </td>
           <td> <input type="checkbox" name="ticked_checks[]" value="lid_closed"> </td>
+          <td> try to "Close lid" again using <a href="http://fact-project.org/smartfact/index.html?sound#control-lid">smartfact</a>.
+          If it works, note problem in logbook. If it still does not work, call dneise.
+          </td>
         </tr>
         <tr>
           <td>Drive Off ?</td>
           <td><?php echo drive_power(); ?></td>
           <td> <input type="checkbox" name="ticked_checks[]" value="drive_off"> </td>
+          <td> try to "Toggle Drive" again using <a href="http://fact-project.org/smartfact/index.html?sound#control-drive">smartfact</a>.
+          Wait a bit after the command. If it still does not work, but telescope is Locked report via email, else call expert.
+          </td>
         </tr>
         <tr>
           <td>TPoint Camera Off?</td>
-          <td> - check manually -  </td>
+          <td> check if TPoint cam is Off on <a href="http://10.0.100.230/ov.html">This power switch - port 2</a>. </td>
+          <td> If not Off, switch it off. If the page does not load - make sure you use VPN. if still not and you did not switch it on during the night; just report via email. If you think it might be on, because you used it, call expert.</td>
           <td> <input type="checkbox" name="ticked_checks[]" value="tpoint_off"> </td>
-        </tr>
-        <tr>
-          <td>Nominated Checker's Email</td>
-          <td> <input type='text' name='Email' /> </td>
         </tr>
       </table>
 
